b43601679d71ac7000d290222747f3f0c5c48a71
Alexander <protonull@protonmail.com>
2024-02-01 03:06:52 +0000

    Remove user route

--- src/main/kotlin/sh/okx/bankoficenia/backend/plugins/Routing.kt
+++ src/main/kotlin/sh/okx/bankoficenia/backend/plugins/Routing.kt
@@ -13,11 +13,9 @@ import sh.okx.bankoficenia.backend.database.SqlUserDao
 import sh.okx.bankoficenia.backend.routes.discordLoginRoute
 import sh.okx.bankoficenia.backend.routes.htmxRoutes
 import sh.okx.bankoficenia.backend.routes.templatedRoutes
-import sh.okx.bankoficenia.backend.routes.userInfoRoute
 
 fun Application.configureRouting(httpClient: HttpClient, sessionDao: SqlSessionDao, userDao: SqlUserDao, accountDao: SqlAccountDao, ledgerDao: SqlLedgerDao, webhook: String, admin: Long) {
     routing {
-        userInfoRoute(httpClient)
         discordLoginRoute(httpClient, sessionDao, userDao)
         templatedRoutes(userDao, accountDao, ledgerDao, httpClient, webhook, admin)
         resources()
